a2d6210db896e34d04abcc243f30e56bf5d81671
4ashk1n <dimalantern@gmail.com>
2026-05-09 16:18:14 +0000

    removed useless redirect

--- src/pages/Landing/ui/index.tsx
+++ src/pages/Landing/ui/index.tsx
@@ -1,20 +1,19 @@
-import { Center, Stack } from "@mantine/core"
-import ArticleList from "../../../widgets/ArticleList/ui"
 import { useEffect } from "react"
 import { useNavigate } from "react-router"
 
 const LandingPage = () => {
     const navigate = useNavigate()
     useEffect(() => {
-        navigate('/article/1')
+        navigate('/feed')
     }, [])
-    return (
-        <Center w='100%'>
-            <Stack maw={'1280px'} w='90%'>
-                <ArticleList />
-            </Stack>
-        </Center>
-    )
+    return null
+    // return (
+    //     <Center w='100%'>
+    //         <Stack maw={'1280px'} w='90%'>
+    //             <ArticleList />
+    //         </Stack>
+    //     </Center>
+    // )
 }
 
 export default LandingPage
